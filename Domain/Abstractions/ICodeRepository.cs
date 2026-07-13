12d996efb6e6ec33ff097ff9373f67cc3d7999a4
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-07-13 00:18:30 -0500

    Add ICodeRepository interface

---
+++ Domain/Abstractions/ICodeRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Abstractions {
+  public interface ICodeRepository<TEntity> where TEntity: class
+  {
+    Task<TEntity?> GetByCodeAsync(string code);
+    Task<bool> ExistsWithCodeAsync(string code);
+  }
+}
--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee02d99f0a0142ba7bea21ca6ab0ce3a36eecc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
