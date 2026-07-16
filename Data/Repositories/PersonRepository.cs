149db10d709ee9c2b975007f27bba15fea0a78d5
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-07-15 21:04:59 -0500

    feat: add Data project with ApplicationDbContext and PersonRepository for data access functionality

---
+++ Data/Repositories/PersonRepository.cs
@@ -0,0 +1,69 @@
+﻿using Domain;
+using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+  public class PersonRepository: IRepository<PersonEntity, Guid>
+  {
+    private readonly ApplicationDbContext _context;
+
+    public PersonRepository(ApplicationDbContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<PersonEntity?> GetByIdAsync(Guid id)
+    {
+      return await _context.Persons.FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public async Task<IEnumerable<PersonEntity>> GetAllAsync()
+    {
+      return await _context.Persons.AsNoTracking()
+                                  .OrderBy(p => p.FirstName)
+                                  .ThenBy(p => p.LastName)
+                                  .ToListAsync();
+    }
+
+    public async Task AddAsync(PersonEntity entity)
+    {
+      if (entity == null)
+      {
+        throw new ArgumentNullException(nameof(entity));
+      }
+
+      await _context.Persons.AddAsync(entity);
+    }
+
+    public Task UpdateAsync(PersonEntity entity)
+    {
+      if (entity == null)
+      {
+        throw new ArgumentNullException(nameof(entity));
+      }
+
+      _context.Persons.Update(entity);
+      return Task.CompletedTask;
+    }
+
+
+    public Task DeleteAsync(PersonEntity entity)
+    {
+      if (entity == null)
+      {
+        throw new ArgumentNullException(nameof(entity));
+      }
+
+      _context.Persons.Remove(entity);
+
+      return Task.CompletedTask;
+    }
+
+
+    public async Task<int> SaveChangesAsync()
+    {
+      return await _context.SaveChangesAsync();
+    }
+  }
+}
