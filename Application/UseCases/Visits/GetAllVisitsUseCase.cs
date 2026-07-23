47d64bea6020be534ee2320d807c5c4c03f904d8
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-07-22 23:10:31 -0500

    feat: add use cases for retrieving active visits, all visits, and visits by person

---
+++ Application/UseCases/Visits/GetAllVisitsUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Domain.Abstractions;
+
+namespace Application.UseCases.Visits
+{
+  public class GetAllVisitsUseCase
+  {
+    public readonly IRepository<VisitEntity, Guid> _repository; 
+
+    public GetAllVisitsUseCase(IRepository<VisitEntity, Guid> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<IEnumerable<VisitEntity>> ExecuteAsync()
+    {
+      return await _repository.GetAllAsync();
+    }
+  }
+}
---
+++ Application/UseCases/Visits/GetVisitsByPersonUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Domain.Abstractions;
+
+namespace Application.UseCases.Visits
+{
+  public class GetVisitsByPersonUseCase
+  {
+    private readonly IVisitRepository<VisitEntity> _visitRepository;
+
+    public GetVisitsByPersonUseCase(IVisitRepository<VisitEntity> visitRepository)
+    {
+      _visitRepository = visitRepository;
+    }
+
+    public async Task<IEnumerable<VisitEntity>> ExecuteAsync(Guid personId)
+    {
+      return await _visitRepository.GetVisitsByPersonIdAsync(personId);
+    }
+  }
+}
